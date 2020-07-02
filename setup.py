7df1d5a46c6bdfb620f241ba086a460d7f133162
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2020-07-02 13:09:36 +0800

    完善注释

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r",encoding='utf-8') as fh:
 
 setuptools.setup(
     name="PictureCombiner",
-    version="1.0.0",
+    version="1.0.1",
     author="YangJinming",
     author_email="944596544@qq.com",
-    description="提供重叠图片拼接功能",
+    description="提供图片拼接功能",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yangjinming1062/PictureCombiner",
